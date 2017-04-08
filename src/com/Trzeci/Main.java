ef4904e6e31111879cefa374b4e2ac51547808ba
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-08 20:02:14 +0200

    RESTART

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -1,11 +1,1 @@
-package com.Trzeci;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        int i;
-        String string;
-        System.out.print("Siema ziomeczku");
-    }
-}
