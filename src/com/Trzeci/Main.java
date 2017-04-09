8b9e5ab02a8d6d55bd261832ef993dbb6f8c0fc9
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-10 00:35:44 +0200

    gettery i settery cd

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -6,16 +6,11 @@ public class Main{
     private int wiek;
     private String color;
         public static void main(String[] args){
-            int[] arr = {1,2,3,4,5};
-                System.out.println("arr[2] " + arr[2]);
-                System.out.println("Wielkosc tablicy " + arr.length);
-            int multi[][] = {{1,2,3},{4,5,6}};
-            System.out.println("multi[1][1]) = " + multi[1][1]);
-
             Main obiekt = new Main();
-             //gettery i settery
-
+            obiekt.setWiek(23);
+            System.out.println(obiekt.getWiek());
         }
+    //gettery i settery
     public int getWiek(){
         return wiek;
     }
