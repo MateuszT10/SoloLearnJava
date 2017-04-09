f9955ddce69a1253576e4fa7207695f52bfa22fe
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-10 00:57:14 +0200

    statyczna zmienna

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class Main{
     private int wiek;
     private String color;
+    public static int LICZNIK;
     Main(){
         wiek = 23;
         color = "Niebieski";
+        LICZNIK++;
     }
     Main(int w){
         wiek=w;
+        LICZNIK++;
     }
     public static void main(String[] args){
         Main a = new Main();
         Main b = new Main(24);
         System.out.println(a.getWiek());
         System.out.println(b.getWiek());
+        System.out.println("Liczba obiektow to " +  LICZNIK);
     }
     //gettery i settery
     public int getWiek(){
