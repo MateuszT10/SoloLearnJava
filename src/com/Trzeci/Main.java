45b24fb333f5456cb6e502d78ff1c924bb76ed04
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-10 00:00:05 +0200

    foreach

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -6,7 +6,10 @@ public class Main{
     public static void main(String[] args){
 
         int[] arr = {1,2,3,4,5};
-        System.out.println("Siemka " + arr[2]);
-        System.out.println("Wielkosc tablicy " + arr.length);
+            System.out.println("arr[2] " + arr[2]);
+            System.out.println("Wielkosc tablicy " + arr.length);
+        for(int i: arr){
+            System.out.println(i);
+        }
     }
 }
