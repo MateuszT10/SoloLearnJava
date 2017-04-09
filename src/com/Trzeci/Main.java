2db72f5e07145f583c826cf57cddce641cb8947e
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-10 00:41:51 +0200

    konstruktory

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Main{
     private int wiek;
     private String color;
-        public static void main(String[] args){
-            Main obiekt = new Main();
-            obiekt.setWiek(23);
-            System.out.println(obiekt.getWiek());
-        }
+    Main(){
+        wiek = 23;
+        color = "Niebieski";
+    }
+    Main(int w){
+        wiek=w;
+    }
+    public static void main(String[] args){
+        Main a = new Main();
+        Main b = new Main(24);
+        System.out.println(a.getWiek());
+        System.out.println(b.getWiek());
+    }
     //gettery i settery
     public int getWiek(){
         return wiek;
@@ -17,4 +25,5 @@ public class Main{
     public void setWiek(int wiek){
         this.wiek = wiek;
     }
+
 }
