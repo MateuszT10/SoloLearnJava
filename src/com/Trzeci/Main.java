649455eed57af45ce962a0b5a1238229347578e1
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-09 23:18:14 +0200

    Odczyt wejscia z klawy

--- src/com/Trzeci/Main.java
+++ src/com/Trzeci/Main.java
@@ -1,8 +1,11 @@
 package com.Trzeci;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Siemka");
+        System.out.println("Siemka napisz teraz cos :)");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Napisales " + scan.nextLine());
     }
-
 }
