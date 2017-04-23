ab0f9753bc478775263d669191a9a1d229c8f990
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-04-23 21:27:37 +0200

    commit

--- src/com/Trzeci/A.java
+++ src/com/Trzeci/A.java
@@ -1,7 +1,7 @@
 package com.Trzeci;
 
 /**
- * Created by Mateusz on 2017-04-10.Git repository
+ * Created by Mateusz on 2017-04-10.Git repository bla bla bls
  */
 public class A {
     protected static int x = 1;
